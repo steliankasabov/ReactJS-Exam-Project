44e0c925d43211965cf998640e981c3c2a539515
Stelian Kasabov <steliankasabov@users.noreply.github.com>
2023-12-18 23:35:34 +0200

    Sofia Park Theatre Festival 1.4

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import styles from './Home.module.css'
 import PlayCarousel from '../../components/PlayCarousel/PlayCarousel';
 import PlaysMap from '../../components/PlaysMap/PlaysMap';
+import About from '../About/About';
 
 export default function Home() {
   return (
@@ -13,21 +14,7 @@ export default function Home() {
     
       <PlayCarousel />
 
-      <div className={styles.descriptionHome}>
-
-      <p><strong>The Sofia Park Theatre Festival is an innovative cultural event that marries the grandeur of theater with the serene beauty of Sofia's parks. </strong></p>
-        <p>&nbsp;</p>
-        <p>This distinctive festival showcases a wide array of scenic performances, not through live acts, but via large cinema screens strategically placed in various parks across the city. These screenings feature a diverse selection of plays, ranging from timeless Shakespearean classics to modern, cutting-edge productions, offering a cinematic twist to the traditional theater experience.</p>
-        <p>&nbsp;</p>
-        <p>The festival transforms the parks into open-air cinemas, where audiences gather under the canopy of stars and trees to enjoy high-definition broadcasts of renowned theatrical performances. This format allows for an intimate yet communal viewing experience, where the audience can appreciate the nuances of each performance in a relaxed outdoor setting.</p>
-        <p>&nbsp;</p>
-        <p>Apart from the screenings, the festival includes interactive workshops and discussion panels, where attendees can delve deeper into the themes of the plays, learn about the intricacies of theater production, and engage in conversations with experts in the field.</p>
-        <p>&nbsp;</p>
-        <p>These activities provide a comprehensive cultural experience, extending beyond just watching the performances. As dusk falls, the parks come alive with the glow of the big screens, creating a magical atmosphere. The fusion of nature's tranquility and the dynamic storytelling on screen offers a unique cultural experience.</p>
-        <p>&nbsp;</p>
-        <p>With the addition of local food vendors and artisanal markets, the "Sofia Park Theatre Festival" becomes more than just a viewing event; it's a celebration of art, community, and the great outdoors, bringing the splendor of theatrical performances into the heart of nature.</p>
-
-      </div>
+      <About />
 
       <PlaysMap />
      
